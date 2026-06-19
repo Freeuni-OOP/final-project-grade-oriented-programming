d489db965c6f6b97a7f887f8678dbf9447d552a9
gbera23-dev <gbera23@freeuni.edu.ge>
2026-06-19 22:24:33 +0400

    added new service class TransactionAuditService

--- Banking_System/src/main/java/com/oop/web_project/logging/TransactionLoggingAspect.java
+++ Banking_System/src/main/java/com/oop/web_project/logging/TransactionLoggingAspect.java
@@ -3,18 +3,20 @@ package com.oop.web_project.logging;
 import com.oop.web_project.annotations.Deposit;
 import com.oop.web_project.annotations.Transfer;
 import com.oop.web_project.annotations.Withdraw;
+import com.oop.web_project.entities.*;
+import com.oop.web_project.exceptions.cardExceptions.CardNotFoundException;
+import com.oop.web_project.exceptions.transactionExceptions.CurrencyExchangeException;
+import com.oop.web_project.persistence.CardRepository;
+import com.oop.web_project.persistence.CurrencyRepository;
 import com.oop.web_project.persistence.TransactionRepository;
+import com.oop.web_project.services.TransactionAuditService;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.reflect.MethodSignature;
-import org.springframework.expression.ExpressionParser;
-import org.springframework.expression.spel.standard.SpelExpressionParser;
-import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.stereotype.Component;
 
-import java.lang.reflect.Method;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 
 /**
@@ -25,10 +27,10 @@ import java.math.BigDecimal;
 @Component
 public class TransactionLoggingAspect {
 
-    private final TransactionRepository transactionRepository;
+    private final TransactionAuditService transactionAuditService;
 
-    public TransactionLoggingAspect(TransactionRepository transactionRepository) {
-        this.transactionRepository = transactionRepository;
+    public TransactionLoggingAspect(TransactionAuditService transactionAuditService) {
+        this.transactionAuditService = transactionAuditService;
     }
 
     @Around(value = "@annotation(deposit) && args(cardId, amountToAdd, currencyCode)",
@@ -36,11 +38,7 @@ public class TransactionLoggingAspect {
     public Object createDepositTransaction(ProceedingJoinPoint pjp, long cardId, BigDecimal amountToAdd,
                                            String currencyCode, Deposit deposit)
             throws Throwable {
-        System.out.println("amount to deposit start");
-        System.out.println(cardId);
-        System.out.println(amountToAdd);
-        System.out.println(currencyCode);
-        System.out.println("amount to deposit end");
+
         Object obj = pjp.proceed();
 
         return obj;
@@ -52,12 +50,10 @@ public class TransactionLoggingAspect {
                                             long cardId, BigDecimal amountToWithdraw, String currencyCode,
                                             Withdraw withdraw)
             throws Throwable {
-        System.out.println("amount to withdraw start");
-        System.out.println(cardId);
-        System.out.println(amountToWithdraw);
-        System.out.println(currencyCode);
-        System.out.println("amount to withdraw end");
-        return pjp.proceed();
+
+        Object obj = pjp.proceed();
+
+        return obj;
     }
 
     @Around(value="@annotation(transfer) && args(senderCardId, receiverCardId, amount, currencyCode)",
@@ -68,13 +64,9 @@ public class TransactionLoggingAspect {
                                                Transfer transfer)
             throws Throwable {
 
-        System.out.println("amount to transfer start");
-        System.out.println(senderCardId);
-        System.out.println(receiverCardId);
-        System.out.println(amount);
-        System.out.println(currencyCode);
-        System.out.println("amount to transfer end");
+        Object obj = pjp.proceed();
 
-        return pjp.proceed();
+
+        return obj;
     }
 }
---
+++ Banking_System/src/main/java/com/oop/web_project/services/TransactionAuditService.java
@@ -0,0 +1,25 @@
+package com.oop.web_project.services;
+
+import com.oop.web_project.entities.Transaction;
+import com.oop.web_project.entities.TransactionStatus;
+
+import java.math.BigDecimal;
+
+public interface TransactionAuditService {
+    /**
+     * Saves the pending transaction into the database
+     * @param cardId id of the card that takes part in transaction
+     * @param amount amount that is being deposited, withdrew or transferred
+     * @param currencyCode currency code of currency in which transaction is happening
+     * @param description description of the transaction
+     * @return instance of the transaction we have saved
+     */
+    Transaction savePending(long cardId, BigDecimal amount, String currencyCode, String description);
+
+    /**
+     * Updates the status of the transaction
+     * @param transaction transaction whose status we are updating
+     * @param status the status transaction's new status should become
+     */
+    void updateStatus(Transaction transaction, TransactionStatus status);
+}
