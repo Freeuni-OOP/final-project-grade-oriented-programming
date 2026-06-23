21debcd5d7f0bba4c70644605aa022922f284322
denuk24-png <denuk24@freeuni.edu.ge>
2026-06-24 02:44:36 +0400

    Added api integration layer.

---
+++ banking_system_front/src/api/httpClient.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { API_BASE_URL, TOKEN_KEY } from './config';
+
+const httpClient = axios.create({
+  baseURL: API_BASE_URL,
+});
+
+httpClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem(TOKEN_KEY);
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+httpClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const status = error.response?.status;
+    if (status === 401 || status === 403) {
+      localStorage.removeItem(TOKEN_KEY);
+      if (window.location.pathname !== '/login') {
+        window.location.href = '/login';
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default httpClient;
