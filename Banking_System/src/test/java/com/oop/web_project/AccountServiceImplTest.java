695e291238a4a92d46c476f925c0315196330933
gbera23-dev <gbera23@freeuni.edu.ge>
2026-07-11 04:27:26 +0400

    updated database actions and validates test case

--- Banking_System/src/test/java/com/oop/web_project/AccountServiceImplTest.java
+++ Banking_System/src/test/java/com/oop/web_project/AccountServiceImplTest.java
@@ -26,10 +26,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
